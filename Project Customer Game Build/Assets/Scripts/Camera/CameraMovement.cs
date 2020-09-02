70eea918f282028c348ef6ea0f04e34cb2190879
Freek Michielsen <480709@student.saxion.nl>
2020-09-02 17:56:40 +0200

    Add trash

---
+++ Project Customer Game Build/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform cameraTransform;
+    Quaternion lookRotation;
+    float xRotation;
+
+    void Start()
+    {
+        xRotation = cameraTransform.rotation.eulerAngles.x;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void SplitQuaternion()
+    {
+        
+    }
+        
+}
