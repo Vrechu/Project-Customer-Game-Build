0be23407a8f0689dccb422b188993e7d4bca66b8
Freek Michielsen <480709@student.saxion.nl>
2020-09-02 18:31:19 +0200

    Add scoring system

---
+++ Project Customer Game Build/Assets/Scripts/ManageScore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageScore : MonoBehaviour
+{
+    public int score = 0;
+
+    void Awake()
+    {
+        TrashPickup.OnPickupByPlayer += AddScore;
+    }
+
+    void OnDestroy()
+    {
+        TrashPickup.OnPickupByPlayer -= AddScore;
+    }
+
+    void Start()
+    {
+        Debug.Log("Score: " + score);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Adds a point to the score.
+    /// </summary>
+    void AddScore()
+    {
+        score++;
+        Debug.Log("Score: " + score);
+    }
+}
---
+++ Project Customer Game Build/Assets/Scripts/Trash/TrashPickup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashPickup : MonoBehaviour
+{
+    public static event Action OnPickupByPlayer;
+    public static event Action OnPickupByMonster;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    /// <summary>
+    /// Gets called when on a collision. 
+    /// Checks who the collector is and then destroys the gameObject.
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        CheckCollector(other.tag);
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Invokes an event depending on tag.
+    /// </summary>
+    /// <param name="tag"></param>
+    void CheckCollector(string tag)
+    {
+        if (tag == "Player")
+        {
+            OnPickupByPlayer?.Invoke();
+        }
+        if (tag == "Monster")
+        {
+           OnPickupByMonster?.Invoke();
+        }
+    }
+}
