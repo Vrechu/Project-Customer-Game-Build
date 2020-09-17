8ad5a8346703a847c6015c0f237a34452ea829a6
Unknown <nickvmarwijk@gmail.com>
2020-09-17 11:23:29 +0200

    Added environment lights

---
+++ Project Customer Game Build/Assets/Scripts/LightRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+    public float speed = 0f;
+
+    public bool ForwardY = false;
+
+    public bool ReverseY = false;
+
+    void Update()
+    {
+            if (ForwardY == true)
+        {
+            transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
+        }
+
+        if (ReverseY == true)
+        {
+            transform.Rotate(0, -Time.deltaTime * speed, 0, Space.Self);
+        }
+
+    }
+}
---
+++ Project Customer Game Build/Assets/Scripts/UpAndDown.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpAndDown : MonoBehaviour
+{
+    public AnimationCurve myCurve;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }
+}
